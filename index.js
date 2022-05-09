663d51365122a342e7699e0eb144c0412a2858df
Hossen <mdmitho199911@gmail.com>
2022-05-09 18:51:30 +0600

    Send JWT Token to the server side and set middletier

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-// const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -10,17 +10,50 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req,res,next)=>{
+
+const authHeader = req.headers.authorization
+
+if(!authHeader){
+  return res.status(401).send({message:'unauthorized'})
+}
+const token = authHeader.split(' ')[1];
+
+// console.log('inside verify token', authHeader);
+}
+
+
 app.post('/login',(req,res)=>{
   const user = req.body;
   console.log(user);
   // DANGER: Do not check password here for serious application
   // USE proper process for hashing and checking
   // After completing all authentication related verification, issue JWT token
-  res.send({ success: true });
-  if(user.password === '123456'){
+  
+  if(user.email === 'user@gmail.com' && user.password === '123456'){
+    const accessToken = jwt.sign({ email: user.email }, process.env.ACCESS_tOKEN_SECRET,{expiresIn:'1h'});
+    res.send({
+      success:true,
+      accessToken:accessToken
+
+    })
       
   }
+  else{
+    res.send({success: false})
+  }
 })
+
+
+app.get('/orders',verifyJWT,(req,res)=>{
+  
+  res.send([
+    { id: 1, item: "sunglass" },
+    { id: 2, item: "glass" }
+  ]);
+})
+
+
 
 app.get('/',(req, res) =>{
     res.send('hello booss')
@@ -31,3 +64,45 @@ app.listen(port, () => {
 })
 
 
+
+
+
+// const verifyJWT = (req, res, next) =>{
+//     const authHeader = req.headers.authorization;
+//     if(!authHeader){
+//         return res.status(401).send({message: 'unauthorized'});
+//     }
+//     const token = authHeader.split(' ')[1];
+//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+//         if(err){
+//             return res.status(403).send({message: 'forbidden'})
+//         }
+//         req.decoded= decoded;
+//         next();
+//     })
+// }
+
+// app.get('/', (req, res) =>{
+//     res.send('Hello from simple JWT Server')
+// });
+
+// app.post('/login', (req, res) =>{
+//     const user = req.body;
+//     console.log(user);
+//     // DANGER: Do not check password here for serious application
+//     
+//     
+//     if(user.email==='user@gmail.com' && user.password === '123456'){
+//         const accessToken = jwt.sign({
+//             email: user.email}, 
+//             process.env.ACCESS_TOKEN_SECRET, 
+//             {expiresIn: '1h'})
+//         res.send({
+//             success: true,
+//             accessToken: accessToken
+//         })
+//     }
+//     else{
+//         res.status(401).send({success: false});
+//     }
+// })
